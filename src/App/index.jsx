4d6c80e60ba90fee8be80bf687bcb5f1ef09e638
Vi <viht61220@gmail.com>
2024-09-30 14:20:33 +0700

    Finish SET-667

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import CounterA from '../CounterA';
 import './App.css';
+import Game from '../Game';
+import CounterA from '../CounterA';
 import { CounterContextProvider } from '../context/CounterContext';
 const App = () => {
 	return (
-		<>
-			<CounterContextProvider>
-				<CounterA />
-			</CounterContextProvider>
-		</>
+		<CounterContextProvider>
+			<CounterA />
+		</CounterContextProvider>
 	);
 };
 
---
+++ src/Square/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Square.css';
+const Square = ({ value, onSquareClick }) => {
+	return (
+		<button
+			className='square'
+			onClick={onSquareClick}
+		>
+			{value}
+		</button>
+	);
+};
+
+export default Square;
